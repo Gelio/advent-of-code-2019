891d4f1c9322a8151fc681addc6ee1f60c3490a9
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-12-12 10:14:05 +0100

    [2020] add ReadLinesFromFile internal function

--- 2020/internal/stdin/stdin.go
+++ 2020/internal/stdin/stdin.go
@@ -8,25 +8,41 @@ import (
 )
 
 // ReadAllLines reads all lines from stdin
-func ReadAllLines() (lines []string, err error) {
-	reader := bufio.NewReader(os.Stdin)
-	var line string
+func ReadAllLines() ([]string, error) {
+	r := bufio.NewReader(os.Stdin)
+
+	return readLinesFromReader(r)
+}
+
+// ReadLinesFromFile reads all lines from an input file
+func ReadLinesFromFile(name string) ([]string, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+
+	r := bufio.NewReader(f)
+
+	return readLinesFromReader(r)
+}
+
+func readLinesFromReader(r *bufio.Reader) ([]string, error) {
+	var lines []string
 
 	for {
-		line, err = reader.ReadString('\n')
+		line, err := r.ReadString('\n')
 
 		if err != nil {
 			if err == io.EOF {
-				err = nil
 				break
 			}
 
-			return
+			return nil, err
 		}
 
 		line = strings.TrimSpace(line)
 		lines = append(lines, line)
 	}
 
-	return
+	return lines, nil
 }
