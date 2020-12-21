72b6d69c9a1c6123fc37fa931365df0faa632578
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-12-21 17:50:31 +0100

    [2020-21] add full backtracking for part A
    Too slow

--- 2020/cmd/21/solve.go
+++ 2020/cmd/21/solve.go
@@ -3,20 +3,16 @@ package main
 // ingredient contains 0 or 1 allergens
 // allergen is in 1 ingredient
 
-// TODO: add a struct instead of having separate methods
-
 func solveA(foods []food) (int, error) {
-	allergensAssignment := make(map[allergenName]ingredientName)
-	ingredientsAssigned := make(map[ingredientName]bool)
-	ingredientEverAssigned := make(map[ingredientName]bool)
+	as := newAssigner(foods)
 
-	assignAllergenRecursive(allergensAssignment, ingredientsAssigned, ingredientEverAssigned, foods)
+	as.assignAllergens(0, 0)
 
 	ingredientCounts := getIngredientsCount(foods)
 
 	sum := 0
 	for ingredient, count := range ingredientCounts {
-		if !ingredientEverAssigned[ingredient] {
+		if !as.ingredientEverAssigned[ingredient] {
 			sum += count
 		}
 	}
@@ -24,61 +20,76 @@ func solveA(foods []food) (int, error) {
 	return sum, nil
 }
 
-func assignAllergenRecursive(allergensAssignment map[allergenName]ingredientName, ingredientsAssigned map[ingredientName]bool, ingredientEverAssigned map[ingredientName]bool, foods []food) bool {
-	if len(foods) == 0 {
-		for ingredient := range ingredientsAssigned {
-			ingredientEverAssigned[ingredient] = true
+type assigner struct {
+	allergensAssignment    map[allergenName]ingredientName
+	ingredientsAssigned    map[ingredientName]bool
+	ingredientEverAssigned map[ingredientName]bool
+	foods                  []food
+}
+
+func newAssigner(foods []food) assigner {
+	var as assigner
+	as.allergensAssignment = make(map[allergenName]ingredientName)
+	as.ingredientsAssigned = make(map[ingredientName]bool)
+	as.ingredientEverAssigned = make(map[ingredientName]bool)
+	as.foods = foods
+
+	return as
+}
+
+func (as *assigner) assignAllergens(foodIndex, allergenIndex int) bool {
+	if foodIndex == len(as.foods) {
+		for ingredient := range as.ingredientsAssigned {
+			as.ingredientEverAssigned[ingredient] = true
 		}
 
 		return true
 	}
 
-	f := foods[0]
-	unassignedAllergen := false
+	if allergenIndex == len(as.foods[foodIndex].allegrens) {
+		return as.assignAllergens(foodIndex+1, 0)
+	}
 
-	// TODO: try to assign a single allergen, and then assign other allergens recursively.
-	// After assigning all alergens, move to the next rule
-	for _, allergen := range f.allegrens {
-		if assignedIngredient, allergenAssigned := allergensAssignment[allergen]; allergenAssigned {
-			// Check if the assigned ingredient appears in the ingredients list
-			hasIngredient := false
-			for _, ingredient := range f.ingredients {
-				if ingredient == assignedIngredient {
-					hasIngredient = true
-					break
-				}
+	allergen := as.foods[foodIndex].allegrens[allergenIndex]
+	f := as.foods[foodIndex]
+
+	if assignedIngredient, allergenAssigned := as.allergensAssignment[allergen]; allergenAssigned {
+		// Check if the assigned ingredient appears in the ingredients list
+		hasIngredient := false
+		for _, ingredient := range f.ingredients {
+			if ingredient == assignedIngredient {
+				hasIngredient = true
+				break
 			}
-			if !hasIngredient {
-				return false
-			}
+		}
+		if !hasIngredient {
+			return false
+		}
+
+		return as.assignAllergens(foodIndex, allergenIndex+1)
+	}
+
+	// unassignedAllergen := true
+
+	// Match unassigned allergen to an unassigned ingredient
+	for _, ingredient := range f.ingredients {
+		if as.ingredientsAssigned[ingredient] {
 			continue
 		}
 
-		unassignedAllergen = true
+		as.allergensAssignment[allergen] = ingredient
+		as.ingredientsAssigned[ingredient] = true
 
-		// Match unassigned allergen to an unassigned ingredient
-		for _, ingredient := range f.ingredients {
-			if ingredientsAssigned[ingredient] {
-				continue
-			}
-
-			allergensAssignment[allergen] = ingredient
-			ingredientsAssigned[ingredient] = true
-
-			if success := assignAllergenRecursive(allergensAssignment, ingredientsAssigned, ingredientEverAssigned, foods[1:]); success {
-				unassignedAllergen = false
-			}
-
-			delete(allergensAssignment, allergen)
-			delete(ingredientsAssigned, ingredient)
+		if success := as.assignAllergens(foodIndex, allergenIndex+1); success {
+			return true
+			// unassignedAllergen = false
 		}
 
-		if unassignedAllergen {
-			return false
-		}
+		delete(as.allergensAssignment, allergen)
+		delete(as.ingredientsAssigned, ingredient)
 	}
 
-	return assignAllergenRecursive(allergensAssignment, ingredientsAssigned, ingredientEverAssigned, foods[1:])
+	return false
 }
 
 func getIngredientsCount(foods []food) map[ingredientName]int {
