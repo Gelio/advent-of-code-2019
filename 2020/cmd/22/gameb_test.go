491d9b463e3cd7c835b83fc24ca5f34e66d758c3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-12-22 21:36:40 +0100

    [2020-22] add benchmark for part B

--- 2020/cmd/22/gameb_test.go
+++ 2020/cmd/22/gameb_test.go
@@ -24,3 +24,19 @@ func TestGameB(t *testing.T) {
 	winningCards := []int{7, 5, 6, 2, 4, 1, 10, 8, 9, 3}
 	assert.Equal(t, winningCards, winner.Cards(), "wrong winning cards")
 }
+
+func BenchmarkGameB(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		p1 := player{
+			ID:   1,
+			deck: newDeck([]int{9, 2, 6, 3, 1}),
+		}
+
+		p2 := player{
+			ID:   2,
+			deck: newDeck([]int{5, 8, 4, 7, 10}),
+		}
+
+		playGameB(&p1, &p2)
+	}
+}
