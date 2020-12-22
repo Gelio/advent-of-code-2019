d0d09c75d5a1af121ef20b8193c8b4124b5788a4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-12-22 21:59:05 +0100

    [2020-22] extract reading input to a separate function
    That function will be used in benchmarks

--- 2020/cmd/22/main.go
+++ 2020/cmd/22/main.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-	"aoc-2020/internal/parse"
-	"aoc-2020/internal/stdin"
-	"aoc-2020/internal/testcases"
 	"fmt"
 )
 
 func main() {
-	input, err := stdin.ReadLinesFromFile("input.txt")
+	d1, d2, err := getPlayerDecksFromInput()
 	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
+		fmt.Println("Error when getting input:", err)
 	}
-
-	playerDefinitions := testcases.SplitTestCaseLines(input)
-
-	player1Cards, err := parse.Ints(playerDefinitions[0][1:])
-	if err != nil {
-		fmt.Println("Error parsing player 1 cards:", err)
-		return
-	}
-	d1 := newDeck(player1Cards)
-
-	player2Cards, err := parse.Ints(playerDefinitions[1][1:])
-	if err != nil {
-		fmt.Println("Error parsing player 2 cards:", err)
-		return
-	}
-	d2 := newDeck(player2Cards)
 
 	fmt.Println("Result A:", solveA(d1, d2))
 	fmt.Println("Result B:", solveB(d1, d2))
--- 2020/cmd/22/solve.go
+++ 2020/cmd/22/solve.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"aoc-2020/internal/parse"
+	"aoc-2020/internal/stdin"
+	"aoc-2020/internal/testcases"
+	"fmt"
+)
+
 func solveA(d1, d2 deck) int {
 	p1Clone, p2Clone := d1.Clone(), d2.Clone()
 	winner := playGameA(&p1Clone, &p2Clone)
@@ -12,4 +19,27 @@ func solveB(d1, d2 deck) int {
 	winner := playGameB(&p1Clone, &p2Clone)
 
 	return winner.Score()
+}
+
+func getPlayerDecksFromInput() (deck, deck, error) {
+	input, err := stdin.ReadLinesFromFile("input.txt")
+	if err != nil {
+		return deck{}, deck{}, fmt.Errorf("cannot read input: %w", err)
+	}
+
+	playerDefinitions := testcases.SplitTestCaseLines(input)
+
+	player1Cards, err := parse.Ints(playerDefinitions[0][1:])
+	if err != nil {
+		return deck{}, deck{}, fmt.Errorf("cannot parse player 1 cards: %w", err)
+	}
+	d1 := newDeck(player1Cards)
+
+	player2Cards, err := parse.Ints(playerDefinitions[1][1:])
+	if err != nil {
+		return deck{}, deck{}, fmt.Errorf("cannot parse player 2 cards: %w", err)
+	}
+	d2 := newDeck(player2Cards)
+
+	return d1, d2, nil
 }
