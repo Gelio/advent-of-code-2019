084c35f120586babe9ae40e63967b8280472cbdb
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-01-02 08:56:31 +0100

    [2019-09] complete part B

--- 2019/09/src/main.rs
+++ 2019/09/src/main.rs
@@ -1,14 +1,21 @@
 use std::{cell::RefCell, fs::read_to_string, rc::Rc};
 
+use aoc_2019_05::Computer;
+
 fn main() {
     let input = read_to_string("input.txt").unwrap();
-    let program = input
+    let program: Vec<isize> = input
         .trim()
         .split(",")
         .map(|x| x.parse().expect(&format!("Cannot parse {}", x)))
         .collect();
-    let mut computer = aoc_2019_05::Computer::new(program, Rc::new(RefCell::new(vec![1])));
+    let mut computer = Computer::new(program.clone(), Rc::new(RefCell::new(vec![1])));
     computer.run_till_halt();
 
     println!("Result A: {:?}", computer.output.borrow());
+
+    let mut computer = Computer::new(program, Rc::new(RefCell::new(vec![2])));
+    computer.run_till_halt();
+
+    println!("Result B: {:?}", computer.output.borrow());
 }
