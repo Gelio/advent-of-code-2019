0139b92411fa02d26ae57a3692a698d3fd1db9c1
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-01-06 15:44:15 +0100

    [2022/20 part 2] Add solution
    It was fairly easy. I was already using a suitable data structure for
    this task (a linked list).
    
    ```sh
    15:44 $ time cargo run --release
        Finished release [optimized] target(s) in 0.00s
         Running `target/release/aoc-2022-20`
    Part 1: 4267
    Part 2: 6871725358451
    
    real    0m0,512s
    user    0m0,508s
    sys     0m0,004s
    ```

--- 2022/20/src/main.rs
+++ 2022/20/src/main.rs
@@ -1,14 +1,15 @@
-use crate::mixer::part_1;
+use crate::mixer::{part_1, part_2};
 
 mod linked_list;
 mod mixer;
 
 fn main() {
-    let numbers: Vec<i32> = include_str!("./input.txt")
+    let numbers: Vec<_> = include_str!("./input.txt")
         .split_whitespace()
         .map(str::parse)
         .collect::<Result<_, _>>()
         .expect("Could not parse some number");
 
-    println!("Part A: {0}", part_1(numbers));
+    println!("Part 1: {0}", part_1(numbers.clone()));
+    println!("Part 2: {0}", part_2(numbers));
 }
--- 2022/20/src/mixer.rs
+++ 2022/20/src/mixer.rs
@@ -1,21 +1,37 @@
 use crate::linked_list::List;
 
-pub fn part_1(numbers: Vec<i32>) -> i32 {
-    let decrypted_numbers = mix(numbers);
+pub fn part_1(numbers: Vec<i64>) -> i64 {
+    let mut list = List::new(numbers);
+    mix(&mut list);
+    let decrypted_numbers = list.into_iter().collect();
     get_grove_coordinates(decrypted_numbers)
 }
 
-fn mix(numbers: Vec<i32>) -> Vec<i32> {
-    let mut list = List::new(numbers);
+const DECRYPTION_KEY: i64 = 811589153;
+
+pub fn part_2(numbers: Vec<i64>) -> i64 {
+    let mut list = List::new(
+        numbers
+            .into_iter()
+            .map(|num| num * DECRYPTION_KEY)
+            .collect(),
+    );
+    for _ in 0..10 {
+        mix(&mut list);
+    }
+
+    let decrypted_numbers = list.into_iter().collect();
+    get_grove_coordinates(decrypted_numbers)
+}
+
+fn mix(list: &mut List<i64>) {
     for item in list.initially_ordered_list_items().clone().iter() {
         let value = item.borrow().value;
         list.move_item(item, value as isize);
     }
-
-    list.into_iter().collect()
 }
 
-fn get_grove_coordinates(decrypted_numbers: Vec<i32>) -> i32 {
+fn get_grove_coordinates(decrypted_numbers: Vec<i64>) -> i64 {
     let (first_zero_index, _) = decrypted_numbers
         .iter()
         .enumerate()
@@ -35,7 +51,12 @@ mod tests {
     #[test]
     fn mixes() {
         let numbers = vec![1, 2, -3, 3, -2, 0, 4];
-        assert_eq!(mix(numbers), vec![1, 2, -3, 4, 0, 3, -2]);
+        let mut list = List::new(numbers);
+        mix(&mut list);
+        assert_eq!(
+            list.into_iter().collect::<Vec<_>>(),
+            vec![1, 2, -3, 4, 0, 3, -2]
+        );
     }
 
     #[test]
